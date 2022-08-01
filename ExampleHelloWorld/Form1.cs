5c856c1891ee4b1219cc2ebb2b8013364dd314aa
franzmoll <56224655+franzmoll@users.noreply.github.com>
2022-08-01 11:14:24 +0200

    Projektdateien hinzufügen.

---
+++ ExampleHelloWorld/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExampleHelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "ExampleHelloWorld";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = "Current Time: " + HelloWorldClassLib.TestClass.SystemTime();
+        }
+    }
+}
---
+++ HelloWorldClassLib/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldClassLib
+{
+    public static class TestClass
+    {
+        public static string SystemTime()
+        {
+            return System.DateTime.Now.ToLongTimeString();
+        }
+    }
+}
